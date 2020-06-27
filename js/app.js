1e26194aceb562bd0f8bc2fa6a3550caaf17ef08
apcaplan <apcaplan@gmail.com>
2020-06-27 17:46:11 -0400

    feat: highlight section in viewport on scroll
    - Add function to highlight section in viewport on scroll

--- js/app.js
+++ js/app.js
@@ -4,8 +4,39 @@ const navMenu = document.getElementById('navbar-menu')
 console.log(navElements)
 
 // Create navbar menu from navElements
-navElements.forEach(element => {
+function buildNav () {
+  navElements.forEach(element => {
   const elementText = document.querySelector(`#${element.id} h2`).innerHTML
   const menuElement = `<li class='navbar-item ${element.className}'><a href="#${element.id}">${elementText}</li>`
   navMenu.insertAdjacentHTML('beforeend', menuElement)
-})
+  })
+}
+
+// Determine if element is in viewport
+function checkVisible (element) {
+  const bounding = element.getBoundingClientRect()
+  return (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  )
+}
+
+// Add "active" classname to element in viewport
+function setActive() {
+  navElements.forEach( el => {
+    if (checkVisible(el)) {
+      el.firstElementChild.classList.add('active')
+    } else {
+      el.firstElementChild.classList.remove('active')
+    }
+  })
+}
+
+
+// add Event listeners
+window.addEventListener('scroll', (event) => setActive())
+
+// Build the navbar
+buildNav()
