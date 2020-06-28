3602d5f8e609d1be11bcaf6f5801ffca64a5fccc
apcaplan <apcaplan@gmail.com>
2020-06-28 16:32:07 -0400

    feat: add responsive styling

--- js/app.js
+++ js/app.js
@@ -1,56 +1,51 @@
-const navElements = document.querySelectorAll('section')
-const navMenu = document.getElementById('navbar-menu')
+const navElements = document.querySelectorAll("section");
+const navMenu = document.getElementById("navbar-menu");
 
 // Create navbar menu from navElements
-function buildNav () {
-  navElements.forEach(element => {
-  const elementText = document.querySelector(`#${element.id} h2`).innerHTML
-  const menuElement = `<li class='navbar-item ${element.className}'><a href="#${element.id}">${elementText}</li>`
-  navMenu.insertAdjacentHTML('beforeend', menuElement)
-  })
+function buildNav() {
+  navElements.forEach((element) => {
+    const elementText = document.querySelector(`#${element.id} h2`).innerHTML;
+    const menuElement = `<li class='navbar-item ${element.className}'><a href="#${element.id}">${elementText}</li>`;
+    navMenu.insertAdjacentHTML("beforeend", menuElement);
+  });
 }
 
 // Determine if element is in viewport
-function checkVisible (element) {
-  const bounding = element.getBoundingClientRect()
-  return (
-    bounding.top >= 0 &&
-    bounding.left >= 0 &&
-    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-  )
+function checkVisible(element) {
+  const bounding = element.getBoundingClientRect();
+  return bounding.top >= 0 && bounding.left >= 0 && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) && bounding.right <= (window.innerWidth || document.documentElement.clientWidth);
 }
 
 // Add "active" classname to element in viewport
 function setActive() {
-  navElements.forEach( el => {
+  navElements.forEach((el) => {
     if (checkVisible(el)) {
-      el.firstElementChild.classList.add('active')
+      el.firstElementChild.classList.add("active");
     } else {
-      el.firstElementChild.classList.remove('active')
+      el.firstElementChild.classList.remove("active");
     }
-  })
+  });
 }
 
 // Scroll to top function
-function scrollToTop () {
-  window.scrollTo(0,0)
+function scrollToTop() {
+  window.scrollTo(0, 0);
 }
 
 // Show scroll to top button
-function scrollButton () {
+function scrollButton() {
   if (document.documentElement.scrollTop > 100 || document.body.scrollTop > 100) {
-    document.getElementById('scroll').style.display = 'flex'
+    document.getElementById("scroll").style.display = "flex";
   } else {
-    document.getElementById('scroll').style.display = 'none'
+    document.getElementById("scroll").style.display = "none";
   }
 }
 
 // add Event listeners
-window.addEventListener('scroll', (event) => {
-  setActive()
-  scrollButton()
-})
+window.addEventListener("scroll", (event) => {
+  setActive();
+  scrollButton();
+});
 
 // Build the navbar
-buildNav()
+buildNav();
